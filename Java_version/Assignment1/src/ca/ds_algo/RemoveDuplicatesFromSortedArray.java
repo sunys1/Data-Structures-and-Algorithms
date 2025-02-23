c1edfb6c3bb406aa4e4ff7fd3d7d4e1f66a5587d
Yizhou Sun <yizhou.sun78@gmail.com>
2025-02-22 23:54:46 -0500

    Solved Q26 using the slow-fast pointer approach

--- Java_version/Assignment1/src/ca/ds_algo/RemoveDuplicatesFromSortedArray.java
+++ Java_version/Assignment1/src/ca/ds_algo/RemoveDuplicatesFromSortedArray.java
@@ -2,7 +2,24 @@ package ca.ds_algo;
 
 /***
  * https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ *
+ * Time: O(N) one-pass
+ * Space: O(1)
  */
 public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        // Use a slow and a fast pointer.
+        int slow = 0, fast = 0;
+        for(fast = 0; fast < nums.length; fast++){
+            //Let index fast increment first and check if the indexed element is different than the element at index slow
+            //If a different element is found at fast, increment slow and update it to the new value to form a non-repetitive array
+            if(nums[fast] != nums[slow]){
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
 
+        //The first k element = nums[0...slow]
+        return slow+1;
+    }
 }
