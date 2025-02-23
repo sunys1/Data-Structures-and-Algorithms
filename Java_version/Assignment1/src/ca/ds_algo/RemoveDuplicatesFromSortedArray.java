b947d887cb6978e11a2ad46e7f8ca0cbca0cd6e6
Yizhou Sun <yizhou.sun78@gmail.com>
2025-02-23 00:09:42 -0500

    Solved Q27 using slow-faster pointer approach

--- Java_version/Assignment1/src/ca/ds_algo/RemoveDuplicatesFromSortedArray.java
+++ Java_version/Assignment1/src/ca/ds_algo/RemoveDuplicatesFromSortedArray.java
@@ -1,7 +1,7 @@
 package ca.ds_algo;
 
 /***
- * https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
  *
  * Time: O(N) one-pass
  * Space: O(1)
@@ -14,6 +14,8 @@ public class RemoveDuplicatesFromSortedArray {
             //Let index fast increment first and check if the indexed element is different than the element at index slow
             //If a different element is found at fast, increment slow and update it to the new value to form a non-repetitive array
             if(nums[fast] != nums[slow]){
+                // Here we increment then update without the concern of OutOfBound exception
+                // because slow is always behind fast
                 slow++;
                 nums[slow] = nums[fast];
             }
---
+++ Java_version/Assignment1/src/ca/ds_algo/RemoveElement.java
@@ -0,0 +1,26 @@
+package ca.ds_algo;
+
+/***
+ * https://leetcode.com/problems/remove-element/description/
+ *
+ * Time: O(N)
+ * Space: O(1)
+ *
+ * Same approach as applied in Q26 with slight variation.
+ */
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        //Use a slow pointer and a fast pointer
+        int slow = 0, fast = 0;
+        while(fast < nums.length){
+            if(nums[fast] != val){
+                // Here we update value then increment because slow could be out of bound.
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+
+        return slow;
+    }
+}
