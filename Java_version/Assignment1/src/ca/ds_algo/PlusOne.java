c753d3062ef6dec57e85cf5f06e28e14ccfb524c
Yizhou Sun <yizhou.sun78@gmail.com>
2025-02-24 14:59:47 -0500

    Solved Q66 using one-pass backward for-loop

---
+++ Java_version/Assignment1/src/ca/ds_algo/PlusOne.java
@@ -0,0 +1,24 @@
+package ca.ds_algo;
+
+/***
+ * https://leetcode.com/problems/plus-one/description/
+ *
+ * Time: O(N)
+ * Space: O(1) for most cases; O(N) worst case if all digits are 9.
+ */
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--){
+            if (digits[i] == 9){
+                digits[i] = 0;
+            }else{
+                digits[i]++;
+                return digits;
+            }
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
