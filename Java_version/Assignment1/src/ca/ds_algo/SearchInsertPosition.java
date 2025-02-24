8f7a86bdd027bad69a0a9d89fb4b375ca0978d2a
Yizhou Sun <yizhou.sun78@gmail.com>
2025-02-24 14:26:26 -0500

    Solved Q35 using binary search

---
+++ Java_version/Assignment1/src/ca/ds_algo/SearchInsertPosition.java
@@ -0,0 +1,31 @@
+package ca.ds_algo;
+
+/***
+ * https://leetcode.com/problems/search-insert-position/description/
+ *
+ * Time: O(logN)
+ * Sapce: O(1)
+ *
+ * Problem description requires a solution with O(logN).
+ * Binary search is known for best case O(1) and average case/worst case O(logN).
+ */
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        // Use binary search
+        int left = 0, right = nums.length - 1; // [left, ... right] closed search interval
+
+        while(left <= right){
+            int middle = (left + right) / 2; // or left + (right - left) / 2 to prevent overflow in case of very large values
+            if (nums[middle] == target){
+                return middle;
+            }else if (nums[middle] < target){
+                left = middle + 1;
+            }else {
+                right = middle - 1;
+            }
+        }
+
+        // Now left = right + 1: nums[right] < target <= nums[left]
+        return left;
+    }
+}
