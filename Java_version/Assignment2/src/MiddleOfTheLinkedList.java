404bfcb668faf2e74218f984f8ffed566f412ae1
Yizhou Sun <yizhou.sun78@gmail.com>
2025-02-28 16:47:10 -0500

    Solved Q876 using slow - fast pointer approach

---
+++ Java_version/Assignment2/src/MiddleOfTheLinkedList.java
@@ -0,0 +1,22 @@
+/***
+ * 876. https://leetcode.com/problems/middle-of-the-linked-list/description/
+ *
+ * Time: O(N)
+ * Space: O(1)
+ *
+ * Use a slow and fast pointer.
+ */
+public class MiddleOfTheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        // Fast pointer advances 2 times faster than the slow pointer
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // When fast points to null, slow is at the middle node of an odd length list
+        // or at the second middle node of an even length list.
+        return slow;
+    }
+}
