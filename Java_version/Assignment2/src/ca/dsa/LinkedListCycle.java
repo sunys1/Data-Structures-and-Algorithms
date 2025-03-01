a7b8815079516d0805ef51660849b74c53336a47
Yizhou Sun <yizhou.sun78@gmail.com>
2025-02-28 23:25:24 -0500

    Solved Q141 using slow-fast pointer approach

---
+++ Java_version/Assignment2/src/ca/dsa/LinkedListCycle.java
@@ -0,0 +1,24 @@
+package ca.dsa;
+
+/***
+ * 141. Linked List Cycle
+ *
+ * Use a slow and fast pointer to find the cycle.
+ * Since fast moves twice the speed of slow, if there is a cycle, they will meet.
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null){return false;};
+
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){return true;}
+        }
+
+        return false;
+    }
+}
